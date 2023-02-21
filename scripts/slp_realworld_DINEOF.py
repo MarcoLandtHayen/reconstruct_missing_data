0d7dc165dd828e7c937fdd93922fab08abfa83ed
MarcoLandtHayen <malaha@gmx.net>
2023-02-21 08:11:45 +0100

    Add rel. loss reduction maps for different patch sizes

--- scripts/slp_realworld_DINEOF.py
+++ scripts/slp_realworld_DINEOF.py
@@ -33,7 +33,7 @@ import os
 feature = 'sea-level-pressure' # Choose either 'sea-level-pressure' or 'sea-surface-temperature' as feature.
 feature_short = 'slp' # Free to set short name, to store results, e.g. 'slp' and 'sst'.
 source = 'realworld' # Choose Earth System Model, either 'FOCI' or 'CESM'.
-mask_type = "fixed"  # Can have random missing values, individually for each data sample ('variable'),
+mask_type = "variable"  # Can have random missing values, individually for each data sample ('variable'),
 # or randomly create only a single mask, that is then applied to all samples identically ('fixed').
 missing_type = "discrete"  # Either specify discrete amounts of missing values ('discrete') or give a range ('range').
 augmentation_factor = 1  # Number of times, each sample is to be cloned, keeping the original order.
@@ -41,23 +41,23 @@ train_val_split = 0.8  # Set rel. amount of samples used for training.
 scale_to = "zero_one"  # Choose to scale inputs to [-1,1] ('one_one') or [0,1] ('zero_one') or 'norm' to normalize inputs or 'no' scaling.
 
 # Set run name:
-run = 'run_4' 
+run = 'run_7' 
 
 # Specify time steps, lat and lon dimensions:
-n_time = 120
-n_lat = 12 # -1 for full data, when working with real world slp fields
-n_lon = 12
+n_time = 900
+n_lat = -1 # -1 for full data, when working with real world slp fields
+n_lon = 144
 
-# Specify number of EOFs to consider for reconstruction:
-n_eof = 10
+# Specify max. number of EOFs to consider for reconstruction:
+max_eofs = 900
 
-# Specify max. number of iterations:
-n_iter = 1000000
+# Specify number of iterations, individually for each rate of missing values:
+n_iters = [5]#[200,100,30,20,10]
 
 # Set further parameters:
 seed = 1  # Seed for random number generator, for reproducibility of missing value mask.
 seed_reserved = 4 # Additional seed to create independent mask of reserved grid points.
-missing = 0.2 # Set rate of missing values.
+missing_values = [0.5]#[0.99,0.95,0.9,0.75,0.5] # Set rates of missing values.
 reserved = 0.1 # Set rate of valid values reserved for cross-validation.
 
 # Get path to store results to:
@@ -80,11 +80,11 @@ parameters = {
     "n_time": n_time,
     "n_lat": n_lat,
     "n_lon": n_lon,
-    "n_eof": n_eof,
-    "n_iter": n_iter,
+    "max_eofs": max_eofs,
+    "n_iters": n_iters,
     "seed": seed,
     "seed_reserved": seed_reserved,
-    "missing": missing,
+    "missing_values": missing_values,
     "reserved": reserved,    
 }
 
@@ -153,74 +153,95 @@ else:
     # Additionally extract values, to have np.array:
     feature_anomaly_flat_zeromean = (feature_anomaly_flat - feature_anomaly_flat.mean(axis=0)).values
     
+
+## Look for optimal number of EOFs to include, for each rate of missing values.
+
+# Initialize storage for total loss and loss for reserved and missing grid points.
+# Dimension: (#missing rates,max_eofs,n_iter)
+# Note: Number of iterations varies for different missing value rates. Take the maximum, to store ALL results in single np.array.
+total_loss_all = np.zeros((len(missing_values),max_eofs,n_iters[0]))
+reserved_loss_all = np.zeros((len(missing_values),max_eofs,n_iters[0]))
+missing_loss_all = np.zeros((len(missing_values),max_eofs,n_iters[0]))
+
+# Loop over rates of missing values:
+for i in range(len(missing_values)):
     
-
-# Get missing mask, fitting to flat feature anomaly with zero mean.
-# Missing grid points are masked as 'False'.
-missing_mask = create_missing_mask(
-    data=feature_anomaly_flat_zeromean, 
-    mask_type=mask_type, 
-    missing_type=missing_type, 
-    missing_min=missing,
-    missing_max=missing,
-    seed=seed).astype('bool')
-
-# Get mask for grid points that are reserved for cross-validation, fitting to flat feature anomaly with zero mean.
-# To reach the specified 'reserved' rate, need to take the rate of missing values into account.
-# Reserved grid points are masked as 'False'.
-reserved_mask = create_missing_mask(
-    data=feature_anomaly_flat_zeromean, 
-    mask_type=mask_type, 
-    missing_type=missing_type, 
-    missing_min=reserved/(1-missing),
-    missing_max=reserved/(1-missing),
-    seed=seed_reserved).astype('bool')
-
-# Now combine both masks to have reserved_gridpoints as subset of valid grid points (not missing!):
-reserved_gridpoints = (reserved_mask==0) * (missing_mask==1)
-
-# And derive missing_gridpoints, for convenience:
-missing_gridpoints = (missing_mask==0)
-
-# Set missing values to zero:
-feature_anomaly_flat_zeromean_missing = feature_anomaly_flat_zeromean * missing_mask
-
-# Initialize storage for total loss and loss for reserved grid points:
-total_loss = []
-reserved_loss = []
-missing_loss = []
-abs_reconstruction = []
-
-# Start iteration:
-for iter in range(n_iter):
+    # Show status:
+    print('missing rate',i+1,'of',len(missing_values))
     
-    # Perform SVD on flat feature anomaly with zero mean, missing values set to zero:
-    pc, s, eof = sp.linalg.svd(
-        feature_anomaly_flat_zeromean_missing, full_matrices=False
-    )
-
-    # Reconstruct flat feature anomaly:
-    feature_anomaly_flat_zeromean_reconstruct = np.matmul((pc[:,:n_eof] * s[:n_eof]), eof[:n_eof])
+    # Get current rate of missing values:
+    missing = missing_values[i]
     
-    # Update former missing values by reconstructed values:
-    feature_anomaly_flat_zeromean_missing[missing_gridpoints] = feature_anomaly_flat_zeromean_reconstruct[missing_gridpoints]
-  
-    # Get and store reconstruction loss for complete samples, reserved grid points and grid points with missing values:
-    total_loss.append(np.mean((feature_anomaly_flat_zeromean - feature_anomaly_flat_zeromean_reconstruct)**2))
-    reserved_loss.append(np.mean((feature_anomaly_flat_zeromean[reserved_gridpoints] - feature_anomaly_flat_zeromean_reconstruct[reserved_gridpoints])**2))
-    missing_loss.append(np.mean((feature_anomaly_flat_zeromean[missing_gridpoints] - feature_anomaly_flat_zeromean_reconstruct[missing_gridpoints])**2))
+    # Get current number of max. iterations:
+    n_iter = n_iters[i]
+    
+    # Get missing mask, fitting to flat feature anomaly with zero mean.
+    # Missing grid points are masked as 'False'.
+    missing_mask = create_missing_mask(
+        data=feature_anomaly_flat_zeromean, 
+        mask_type=mask_type, 
+        missing_type=missing_type, 
+        missing_min=missing,
+        missing_max=missing,
+        seed=seed).astype('bool')
+
+    # Get mask for grid points that are reserved for cross-validation, fitting to flat feature anomaly with zero mean.
+    # To reach the specified 'reserved' rate, need to take the rate of missing values into account.
+    # Reserved grid points are masked as 'False'.
+    reserved_mask = create_missing_mask(
+        data=feature_anomaly_flat_zeromean, 
+        mask_type=mask_type, 
+        missing_type=missing_type, 
+        missing_min=reserved/(1-missing),
+        missing_max=reserved/(1-missing),
+        seed=seed_reserved).astype('bool')
+
+    # Now combine both masks to have reserved_gridpoints as subset of valid grid points (not missing!):
+    reserved_gridpoints = (reserved_mask==0) * (missing_mask==1)
+
+    # And derive missing_gridpoints, for convenience:
+    missing_gridpoints = (missing_mask==0)
+
+    # Set missing values to zero:
+    feature_anomaly_flat_zeromean_missing = feature_anomaly_flat_zeromean * missing_mask
+    
+    # Loop over number of EOF modes to include for reconstruction:
+    for n_eof in np.arange(1,max_eofs+1):
         
-    # Get and store summed absolute reconstructed missing values:
-    abs_reconstruction.append(np.sum(np.abs(feature_anomaly_flat_zeromean_missing[missing_gridpoints])))
+        # Show status:
+        print('  add EOF',n_eof,'of',max_eofs)
     
-    # Store results every 10,000 iterations:
-    if (iter+1)%100000 == 0:
-        np.save(path_to_store_results / 'total_loss.npy', np.array(total_loss))
-        np.save(path_to_store_results / 'reserved_loss.npy', np.array(reserved_loss))
-        np.save(path_to_store_results / 'missing_loss.npy', np.array(missing_loss))
-        np.save(path_to_store_results / 'abs_reconstruction.npy', np.array(abs_reconstruction))
+        # Initialize storage for total loss and loss for reserved and missing grid points:
+        total_loss = []
+        reserved_loss = []
+        missing_loss = []
+        
+        # Start iteration:
+        for iter in range(n_iter):
 
-        # Set filename to store snapshot of reconstruction after current number of iterations:
-        feature_anomaly_flat_zeromean_missing_filename = 'feature_anomaly_flat_zeromean_missing_'+str(iter+1)+'_iterations.npy'
-        np.save(path_to_store_results / feature_anomaly_flat_zeromean_missing_filename,feature_anomaly_flat_zeromean_missing)
+            # Perform SVD on flat feature anomaly with zero mean, missing values set to zero:
+            pc, s, eof = sp.linalg.svd(
+                feature_anomaly_flat_zeromean_missing, full_matrices=False
+            )
+
+            # Reconstruct flat feature anomaly:
+            feature_anomaly_flat_zeromean_reconstruct = np.matmul((pc[:,:n_eof] * s[:n_eof]), eof[:n_eof])
+
+            # Update former missing values by reconstructed values:
+            feature_anomaly_flat_zeromean_missing[missing_gridpoints] = feature_anomaly_flat_zeromean_reconstruct[missing_gridpoints]
+
+            # Get and store reconstruction loss for complete samples, reserved grid points and grid points with missing values:
+            total_loss.append(np.mean((feature_anomaly_flat_zeromean - feature_anomaly_flat_zeromean_reconstruct)**2))
+            reserved_loss.append(np.mean((feature_anomaly_flat_zeromean[reserved_gridpoints] - feature_anomaly_flat_zeromean_reconstruct[reserved_gridpoints])**2))
+            missing_loss.append(np.mean((feature_anomaly_flat_zeromean[missing_gridpoints] - feature_anomaly_flat_zeromean_reconstruct[missing_gridpoints])**2))
+            
+        # Store losses for current rate of missing values and current number of included EOFs:
+        total_loss_all[i,n_eof-1,:n_iter] = np.array(total_loss)
+        reserved_loss_all[i,n_eof-1,:n_iter] = np.array(reserved_loss)
+        missing_loss_all[i,n_eof-1,:n_iter] = np.array(missing_loss)
+        
+    # Save (interim) results after each rate of missing values:
+    np.save(path_to_store_results / 'total_loss_all.npy', total_loss_all)
+    np.save(path_to_store_results / 'reserved_loss_all.npy', reserved_loss_all)
+    np.save(path_to_store_results / 'missing_loss_all.npy', missing_loss_all)
         
